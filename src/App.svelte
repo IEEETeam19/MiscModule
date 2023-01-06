4d6e21db760c00b0a9d70abb883a0f338b311064
unknown <snehaaes@gmail.com>
2023-01-06 19:53:03 +0530

    first project commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+
+  import Box1 from "./lib/box1.svelte";
+
+
+</script>
+
+<main>
+
+  <Box1/>
+
+</main>
---
+++ src/lib/box1.svelte
@@ -0,0 +1,61 @@
+<script>
+
+</script>
+
+<main class="relative">
+    <div class="mt-10">
+        <div class="bg-teal-600 w-80 h-80 m-auto" style="width: 1163px; height: 390px">
+            <div class="text-white flex justify-between text-sm font-serif">
+                <p>Number filter Criteria</p>
+                <p>Multiples & Divisors</p>
+            </div>
+            <div class="text-white flex text-xs font-serif">
+                <input class="bg-teal-800 w-10 m-2" type="text" name="lowerBound" id="" placeholder="Lower Bound">
+                <input class="bg-teal-800 w-10 m-2" type="text" name="upperBound" id="" placeholder="Upper Bound">
+            </div>
+            <div>
+                <label for="op1" class="text-white text-xs font-serif">
+                    <input type="radio" name="opr" id="op1"> 
+                    Select all Numbers
+                    <p class="text-slate-300 m-1">pick all numbers from the list</p>
+                </label>
+            </div>
+            <div>
+                <label for="op2" class="text-white text-xs font-serif">
+                    <input type="radio" name="opr" id="op2">
+                    Filter only integers
+                    <p class="text-slate-300 m-1">pick all whole numbers from the list</p>
+                </label>
+            </div>
+            <div>
+                <label for="op3" class="text-white text-xs font-serif">
+                    <input type="radio" name="opr" id="op3">
+                    Filter only fractions
+                    <p class="text-slate-300 m-1">pick all decimal numbers from the list</p>
+                </label>
+            </div>
+            <div>
+                <label for="op4" class="text-white text-xs font-serif">
+                    <input type="radio" name="opr" id="op4">
+                    Filter only even numbers
+                    <p class="text-slate-300 m-1">pick all even numbers from the list</p>
+                </label>
+            </div>
+            <div>
+                <label for="op5" class="text-white text-xs font-serif">
+                    <input type="radio" name="opr" id="op5">
+                    Filter only odd numbers
+                    <p class="text-slate-300 m-1">pick all odd numbers from the list</p>
+                </label>
+            </div>
+            <br>    
+            <div>
+                <p class="text-white text-xs font-serif">Number delimiter & sorting</p>
+            </div>
+            <div class="text-white flex text-xs font-serif">
+                <input class="bg-teal-800 w-10 m-2" type="text" name="lowerBound" id="">
+                <input class="bg-teal-800 w-10 m-2" type="text" name="upperBound" id="">
+            </div>
+        </div>
+    </div>
+</main>
